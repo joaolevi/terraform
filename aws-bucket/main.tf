c71e72a5da0b3459709e33c9be820644429c308a
João Levi <jlgl@ic.ufal.br>
2023-03-23 21:16:03 -0300

    Creating a S3 bucket

---
+++ aws-bucket/main.tf
@@ -0,0 +1,22 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+      aws = {
+      source = "hashicorp/aws"
+      version = "4.59.0"
+      }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+
+  default_tags {
+    tags = {
+      owner = "levi"
+      managed-by = "terraform"
+    }
+  }
+}
